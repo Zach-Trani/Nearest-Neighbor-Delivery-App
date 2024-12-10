0390185f2b1da95712fe2694ec2a5b700b3e6402
Zach-Trani <trani.zach@gmail.com>
2024-12-10 15:37:18 -0500

    Add insert method to hash table

--- CreateHashTable.py
+++ CreateHashTable.py
@@ -5,8 +5,28 @@ class CreateHashMap:
         for i in range(initial_capacity):
             self.list.append([])
 
-    # insert function
+    # insert function - inserts a new item into hash table
+        # citing source - "C950 - Webinar 1 - Let's Go Hashing - Complete Python Code"
+    def insert(self, key, item):
+        # get the bucket list where this item will go
+        bucket = hash(key) % len(self.list)
+        bucket_list = self.list[bucket]
 
-    # lookup function
+        # update key if it is already existing in bucket
+        for kv in bucket_list:
+            # print (key_value)
+            if kv[0] == key:
+                kv[1] = item
+                return True
+            
+        # if key does not exist, insert item to the end of the bucket list
+        key_value = [key, item]
+        bucket_list.append(key_value)
+        return True
 
-    # remove function
+
+    # lookup function - looks up a item in hash table
+
+
+
+    # remove function - removes a item in hash table
--- Truck.py
+++ Truck.py
@@ -1,5 +1,4 @@
 class Truck:
   def __init__(self, velocity, package_capacity, package_ID, mileage):
-    self.name = name
-    self.age = age
+    self.
 
