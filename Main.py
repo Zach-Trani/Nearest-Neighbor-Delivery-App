57fa0ed83a3469a39facf73aeb1abb65cc6fc4d6
Zach-Trani <trani.zach@gmail.com>
2024-12-11 21:43:24 -0500

    Fix main method clean up

--- Main.py
+++ Main.py
@@ -23,8 +23,6 @@ with open("Data/Address_Data.csv") as Address_Data_input:
     Address_Data_CSV = list(Address_Data_CSV)
 print(Address_Data_CSV)
 
-# instantiate the hash map
-package_hash_table = CreateHashMap()
 
 # Create package objects from the CSV package file
 # Load package objects into the hash table: package_hash_table
@@ -43,27 +41,41 @@ def load_package_data(filename, package_hash_table):
 
             # Package object
             p = Package(pID, pAddress, pDeadline_time, pCity, pZipcode, pState, pWeight, pStatus)
-
-             # Debug print - add this
-            print(f"Creating package {pID}")
             
             # Insert data into hash table
             package_hash_table.insert(pID, p)
-            
-            # Debug print - add this
-            print(f"After insertion, looking up package {pID}:")
-            print(package_hash_table.lookup(pID))
 
+# Method for finding distance between two addresses
+def distance_in_between(x_val, y_val):
+    distance = Distance_Data_CSV[x_val][y_val]
+    if distance == '':
+        distance = Distance_Data_CSV[y_val][x_val]
 
+    return float(distance)
+
+# Method to get address number from string literal of address
+def extract_address(address):
+    for row in Address_Data_CSV:
+        if address in row[2]:
+            return int(row[0])
+
+# Create truck object truck1
+truck_1 = Truck.Truck(16, 18, None, [1, 13, 14, 15, 16, 20, 29, 30, 31, 34, 37, 40], 0.0, "4001 South 700 East",
+                     datetime.timedelta(hours=8))
+
+# Create truck object truck2
+truck_2 = Truck.Truck(16, 18, None, [3, 6, 12, 17, 18, 19, 21, 22, 23, 24, 26, 27, 35, 36, 38, 39], 0.0,
+                     "4001 South 700 East", datetime.timedelta(hours=10, minutes=20))
+
+# Create truck object truck3
+truck_3 = Truck.Truck(16, 18, None, [2, 4, 5, 6, 7, 8, 9, 10, 11, 25, 28, 32, 33], 0.0, "4001 South 700 East",
+                     datetime.timedelta(hours=9, minutes=5))
+
+# instantiate the hash table
+package_hash_table = CreateHashMap()
+
+# load packages into hash table
 load_package_data("Data/Package_Data.csv", package_hash_table)
-
-
-
-
-
-
-
-
 
 
 
